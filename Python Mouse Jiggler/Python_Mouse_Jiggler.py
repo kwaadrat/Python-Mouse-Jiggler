c27be37a1a6c2dc9270ab782e94f859fd6a86103
kwaadrat <kwaadrat@gmail.com>
2022-12-29 21:38:06 +0100

    naprawiona zmienna

--- Python Mouse Jiggler/Python_Mouse_Jiggler.py
+++ Python Mouse Jiggler/Python_Mouse_Jiggler.py
@@ -3,7 +3,11 @@ import pyautogui
 from time import sleep
 import os
 
-x = input('Insert time in minutes: ')
+x_string = input('Insert time in minutes: ')
+
+pyautogui.hotkey('alt', 'tab')
+
+x = int(x_string)
 
 x = x * 60
 
@@ -12,5 +16,3 @@ for x in range(x):
     pyautogui.moveTo(1901, 1061)
     print(x)
     sleep(1)
-    clear = lambda: os.system('cls')
-    clear()
