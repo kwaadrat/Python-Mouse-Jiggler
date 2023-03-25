f194d41b649f71dcdbf2d83349189e8d99e8b202
kwaadrat <kwaadrat@gmail.com>
2023-03-25 22:34:05 +0100

    fala

--- Python Mouse Jiggler/Python_Mouse_Jiggler.py
+++ Python Mouse Jiggler/Python_Mouse_Jiggler.py
@@ -7,12 +7,17 @@ x_string = input('Insert time in minutes: ')
 
 pyautogui.hotkey('command', 'tab')
 
-x = int(x_string)
+i = int(x_string)
 
-x = x * 60
+i = i * 60
 
-for x in range(x):
-    pyautogui.moveTo(190, 10)
-    pyautogui.moveTo(19, 10)
-    print(x)
+x = 15
+y = 15
+
+for i in range(i):
+    pyautogui.moveTo(x, y)
+    pyautogui.moveTo(x, y)
+    print(i)
     sleep(1)
+    x = x + 1
+    y = y + 1
