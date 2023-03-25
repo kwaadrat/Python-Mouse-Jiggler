ec9cdcd9477485aad4d79f12497629a1df52a1c8
kwaadrat <kwaadrat@gmail.com>
2023-03-25 21:34:01 +0100

    macosmod

--- Python Mouse Jiggler/Python_Mouse_Jiggler.py
+++ Python Mouse Jiggler/Python_Mouse_Jiggler.py
@@ -5,14 +5,14 @@ import os
 
 x_string = input('Insert time in minutes: ')
 
-pyautogui.hotkey('alt', 'tab')
+pyautogui.hotkey('command', 'tab')
 
 x = int(x_string)
 
 x = x * 60
 
 for x in range(x):
-    pyautogui.moveTo(1900, 1060)
-    pyautogui.moveTo(1901, 1061)
+    pyautogui.moveTo(190, 10)
+    pyautogui.moveTo(19, 10)
     print(x)
     sleep(1)
